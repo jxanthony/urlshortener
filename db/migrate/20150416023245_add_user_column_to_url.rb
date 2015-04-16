4aaca36677b08b924755420e191a8d9da80d4628
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-04-16 10:33:51 +0800

    added user_id column to url table
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

---
+++ db/migrate/20150416023245_add_user_column_to_url.rb
@@ -0,0 +1,5 @@
+class AddUserColumnToUrl < ActiveRecord::Migration
+  def change
+  	add_column :urls, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416022538) do
+ActiveRecord::Schema.define(version: 20150416023245) do
 
   create_table "urls", force: :cascade do |t|
     t.text     "long_url"
     t.string   "short_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
