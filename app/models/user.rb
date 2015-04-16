8abdb2bfd22dc6128a89d5e8c3a0892acf7a5bd6
Holloway, Kean Ho <hollowaykeanho@gmail.com>
2015-04-16 11:21:33 +0800

    ported user model validation and testing
    Signed-off-by: Holloway, Kean Ho <hollowaykeanho@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
+	validates_uniqueness_of :email
+	validates_presence_of :email, :name
 	has_many :urls
+
+	def self.authenticate(email, password)
+		begin
+			user = self.find_by_email(email)
+		rescue
+			user = nil
+		ensure
+			return user if not user.nil? and email == user.email and password == user.password
+		end
+		nil
+	end
 end
