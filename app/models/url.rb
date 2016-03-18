8caa87fbefa959621c12945ac25ccd1d64ce89e1
jxanthony <jxanthony@gmail.com>
2016-03-18 11:36:50 -0700

    changes to layout

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,6 @@
 class Url < ActiveRecord::Base
-	validates_presence_of :long_url
+  validates :long_url, format: {with: Regexp.new(URI::regexp(%w(http https)))}, presence: true
+
 	# validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))$/
 	belongs_to :user
 end
